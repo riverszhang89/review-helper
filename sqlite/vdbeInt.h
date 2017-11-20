8d8b57d54eaa179c0cbda34feec69fc89d573556
Rivers Zhang <hzhang320@bloomberg.net>
2017-11-20 15:31:12 +0000

    Add sqlite/vdbeInt.h and sqlite/vdbeaux.c

---
+++ sqlite/vdbeInt.h
@@ -0,0 +1,1 @@
+If the order of members in struct Mem is changed, columnNullValue (defined in sqlite/vdbeapi.c) needs to be reordered accordingly.
---
+++ sqlite/vdbeaux.c
@@ -0,0 +1,1 @@
+If a new fixed-length serial type is introduced, sqlite3IsFixedLengthSerialType (defined in sqlite/vdbeInt.h) needs to be changed accordingly.
