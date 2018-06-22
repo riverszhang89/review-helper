f18f462126b27b533585ecf0e19991ade341c66a
Rivers Zhang <hzhang320@bloomberg.net>
2018-06-22 15:03:31 -0400

    Joe's rule.

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -1,1 +1,2 @@
 Please port the change to cdb2jdbc if necessary.
+If you add any internal configuration state to this file, please update the reset_the_configuration() function as well to include it.
